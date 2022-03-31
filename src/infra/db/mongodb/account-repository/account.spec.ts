7d593860daa0de325aabd7b706617a2c4a479a11
Adan Nogueira <adan.nogueira@gmail.com>
2022-03-31 08:25:03 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -12,6 +12,11 @@ describe('Account Mongodb Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('Should return true on success', async () => {
     const sut = makeSut()
     const account = await sut.add({
