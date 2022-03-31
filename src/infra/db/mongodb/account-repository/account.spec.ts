fe0eef2685f7ae428c2ff01cf393ffe6806bbad4
Adan Nogueira <adan.nogueira@gmail.com>
2022-03-31 08:23:01 -0300

    refactor: move sut creation to a helper method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,6 +1,9 @@
 import { AccountMongoRepository } from './account'
 import { MongoHelper } from './helpers/mongo-helper'
 
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
 describe('Account Mongodb Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -10,7 +13,7 @@ describe('Account Mongodb Repository', () => {
     await MongoHelper.disconnect()
   })
   test('Should return true on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
