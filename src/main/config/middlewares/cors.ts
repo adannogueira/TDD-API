50906d9c2f23dfca00920fa8436a60b6a41d50a2
Adan Nogueira <adan.nogueira@gmail.com>
2022-04-01 08:26:55 -0300

    feat: ensure cors is enabled

---
+++ src/main/config/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.header('access-control-allow-origin', '*')
+  res.header('access-control-allow-headers', '*')
+  res.header('access-control-allow-methods', '*')
+  next()
+}
