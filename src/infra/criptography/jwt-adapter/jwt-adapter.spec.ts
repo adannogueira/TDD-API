98363cc80ccdb8830c975b68fe1c4bd547119564
Adan Nogueira <adan.nogueira@gmail.com>
2022-04-30 17:25:02 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -20,4 +20,12 @@ describe('Jwt Adapter', () => {
     const token = await sut.encrypt('any_id')
     expect(token).toBe('any_token')
   })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign')
+      .mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
