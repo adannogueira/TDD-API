be119542d84cb44caa7a0481e8110c1a2e77ec39
Adan Nogueira <adan.nogueira@gmail.com>
2022-04-30 17:19:46 -0300

    test: ensure BcryptAdapter returns a token on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,23 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  sign: (): string => {
+    return 'any_token'
+  }
+}))
+
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    const token = await sut.encrypt('any_id')
+    expect(token).toBe('any_token')
   })
 })
