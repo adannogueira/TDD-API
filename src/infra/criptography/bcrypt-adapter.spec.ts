7d326df1dac9d5f798820dd4f24ba6de3eabdbac
Adan Nogueira <adan.nogueira@gmail.com>
2022-04-30 15:29:37 -0300

    refactor: rename Encrypter to Hasher

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -17,20 +17,20 @@ describe('Bcrypt Adapter', () => {
   test('Should call Bcrypt with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('any_value')
+    await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on success', async () => {
     const sut = makeSut()
-    const hash = await sut.encrypt('any_value')
+    const hash = await sut.hash('any_value')
     expect(hash).toBe('hash')
   })
 
   test('Should throw if Bcrypt throws', async () => {
     const sut = makeSut()
     bcrypt.hash = jest.fn().mockRejectedValueOnce(new Error())
-    const promise = sut.encrypt('any_value')
+    const promise = sut.hash('any_value')
     await expect(promise).rejects.toThrow()
   })
 })
