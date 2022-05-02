ab5cc556da91d5217fdbd7b614cf1774fc878232
Adan Nogueira <adan.nogueira@gmail.com>
2022-05-02 08:49:07 -0300

    refactor: correct LoginFactory implementation

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,18 +1,25 @@
-import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
-import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
 import { Controller } from '../../../presentation/protocols'
+import env from '../../config/env'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLoginController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
   const accountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeLoginValidation())
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository)
+  const loginController = new LoginController(dbAuthentication, makeLoginValidation())
   const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  return new LogControllerDecorator(loginController, logMongoRepository)
 }
