9bb9ebddd6b2c102e23b91aa5351c50cb7d8b8bd
Adan Nogueira <adan.nogueira@gmail.com>
2022-05-23 11:09:48 -0300

    feat: ensure db add survey calls add survey repository with correct values

---
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -0,0 +1,11 @@
+import { AddSurvey, AddSurveyModel } from '../../../domain/usecases/add-survey'
+import { AddSurveyRepository } from './add-survey-protocols'
+
+export class DbAddSurvey implements AddSurvey {
+  constructor (private readonly addSurveyRepository: AddSurveyRepository) {}
+
+  async add (survey: AddSurveyModel): Promise<void> {
+    await this.addSurveyRepository.add(survey)
+    return null
+  }
+}
