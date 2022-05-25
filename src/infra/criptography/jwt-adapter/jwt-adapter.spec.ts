623f07ec9674dc872113f21fdb61b319706df96c
Adan Nogueira <adan.nogueira@gmail.com>
2022-05-25 08:21:10 -0300

    test: ensure jwt adapter calls verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -24,11 +24,24 @@ describe('Jwt Adapter', () => {
       await expect(promise).rejects.toThrow()
     })
   })
+
+  describe('verify()', () => {
+    test('Should call sign with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
+    })
+  })
 })
 
 jest.mock('jsonwebtoken', () => ({
   sign: (): string => {
     return 'any_token'
+  },
+
+  verify: (): string => {
+    return 'any_id'
   }
 }))
 
