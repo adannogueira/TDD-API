7fb70efdfda38f7d63081cfb860ba5dc5fc8175b
Adan Nogueira <adan.nogueira@gmail.com>
2022-05-25 08:27:47 -0300

    test: ensure jwt adapter returns a decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -32,6 +32,12 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut()
+      const token = await sut.decrypt('any_token')
+      expect(token).toBe('any_id')
+    })
   })
 })
 
