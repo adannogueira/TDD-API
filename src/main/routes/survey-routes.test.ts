4dc23b48c176049841e25edc59d2b8950c454319
Adan Nogueira <adan.nogueira@gmail.com>
2022-05-31 08:37:44 -0300

    test: ensure add survey routes returns 204 when user is authorized

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,13 @@
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
+import env from '../config/env'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 let surveyCollection: Collection
+let accountCollection: Collection
+
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -15,6 +19,8 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
     await surveyCollection.deleteMany({})
   })
 
@@ -32,6 +38,37 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 when user is authorized', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const id = result.insertedId.toString()
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: result.insertedId
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
     })
   })
 })
