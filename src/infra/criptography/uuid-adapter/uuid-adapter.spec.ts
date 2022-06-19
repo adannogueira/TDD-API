f66ae58376d6185dbfdfbf6c6917ecb485182ee2
Adan Nogueira <adan.nogueira@gmail.com>
2022-06-19 17:09:24 -0300

    test: ensure uuid adapter returns a valid uuid

--- src/infra/criptography/uuid-adapter/uuid-adapter.spec.ts
+++ src/infra/criptography/uuid-adapter/uuid-adapter.spec.ts
@@ -1,15 +1,28 @@
 import { IdGenerator } from '../../../data/protocols/criptography/id-generator'
 import { UuidAdapter } from './uuid-adapter'
-import * as uuid from 'uuid'
+import { v4, validate } from 'uuid'
 
-jest.mock('uuid')
+jest.mock('uuid', () => {
+  const original = jest.requireActual('uuid')
+  return {
+    __esModule: true,
+    ...original,
+    v4: jest.fn(() => original.v4())
+  }
+})
 
 describe('UuidAdapter', () => {
   test('Should call uuid v4', () => {
     const sut = makeSut()
-    const generateSpy = jest.spyOn(uuid, 'v4')
     sut.generate()
-    expect(generateSpy).toHaveBeenCalled()
+    expect(v4).toHaveBeenCalled()
+  })
+
+  test('Should return a valid uuid', () => {
+    jest.clearAllMocks()
+    const sut = makeSut()
+    const generatedUuid = sut.generate()
+    expect(validate(generatedUuid)).toBeTruthy()
   })
 })
 
