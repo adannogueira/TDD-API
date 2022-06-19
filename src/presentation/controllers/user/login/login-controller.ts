442eea20b8f40a482fdf29e57028204d8c994788
Adan Nogueira <adan.nogueira@gmail.com>
2022-06-19 18:13:12 -0300

    feat: ensure login controller returns unauthorized if no tokens exist

--- src/presentation/controllers/user/login/login-controller.ts
+++ src/presentation/controllers/user/login/login-controller.ts
@@ -20,11 +20,11 @@ export class LoginController implements Controller {
         return badRequest(error)
       }
       const { email, password } = httpRequest.body
-      const accessToken = await this.authentication.auth({ email, password })
-      if (!accessToken) {
+      const tokens = await this.authentication.auth({ email, password })
+      if (!tokens) {
         return unauthorized()
       }
-      return ok({ accessToken })
+      return ok(tokens)
     } catch (err) {
       return serverError(err)
     }
