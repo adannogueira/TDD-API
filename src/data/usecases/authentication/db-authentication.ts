20c95813f8eb0c5d8f9082f31bc0e2e8735fc6b2
Adan Nogueira <adan.nogueira@gmail.com>
2022-06-19 17:28:56 -0300

    feat: ensure a refresh token is returned from db authentication

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,10 +1,13 @@
 import {
   Authentication,
+  Tokens,
   AuthenticationModel,
   HashComparer,
-  Encrypter,
+  AccessEncrypter,
   LoadAccountByEmailRepository,
-  UpdateAccessTokenRepository
+  UpdateAccessTokenRepository,
+  RefreshEncrypter,
+  IdGenerator
 } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
@@ -12,19 +15,24 @@ export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
     private readonly encrypter: Encrypter,
-    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
+    private readonly idGenerator: IdGenerator
   ) {}
 
-  async auth (authentication: AuthenticationModel): Promise<string> {
+  async auth (authentication: AuthenticationModel): Promise<Tokens> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
     if (account) {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(account.id)
+        const jti = this.idGenerator.generate()
+        const refreshToken = await this.encrypter.encryptRefresh(account.id, jti)
         await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
-        return accessToken
+        return { accessToken, refreshToken }
       }
     }
     return null
   }
 }
+
+interface Encrypter extends AccessEncrypter, RefreshEncrypter {}
