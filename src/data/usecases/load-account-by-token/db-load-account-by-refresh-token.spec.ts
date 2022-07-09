505af7778f9250df0f47e594a021f9aade64b600
Adan Nogueira <adan.nogueira@gmail.com>
2022-07-09 04:33:52 -0300

    test: ensure db load account by refresh token calls decrypt refresh with correct values

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-refresh-token.spec.ts
@@ -0,0 +1,55 @@
+import { AccountModel } from '../../../domain/models/account'
+import { RefreshDecrypter } from '../../protocols/criptography/refresh-decrypter'
+import { DbLoadAccountByRefreshToken } from './db-load-account-by-refresh-token'
+import { LoadAccountByRefreshTokenRepository } from '../../protocols/db/account/load-account-by-refresh-token-repository'
+
+describe('DbLoadAccountByRefreshToken Usecase', () => {
+  test('Should call RefreshDecrypter with correct values', async () => {
+    const { sut, decrypterStub } = makeSut()
+    const decryptSpy = jest.spyOn(decrypterStub, 'decryptRefresh')
+    await sut.load('any_token')
+    expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+})
+
+interface sutTypes {
+  sut: DbLoadAccountByRefreshToken
+  decrypterStub: RefreshDecrypter
+  loadAccountByRefreshTokenRepoStub: LoadAccountByRefreshTokenRepository
+}
+
+const makeSut = (): sutTypes => {
+  const decrypterStub = makeDecrypter()
+  const loadAccountByRefreshTokenRepoStub = makeLoadAccountByRefreshTokenRepo()
+  const sut = new DbLoadAccountByRefreshToken(decrypterStub)
+  return {
+    sut,
+    decrypterStub,
+    loadAccountByRefreshTokenRepoStub
+  }
+}
+
+const makeDecrypter = (): RefreshDecrypter => {
+  class DecrypterStub implements RefreshDecrypter {
+    async decryptRefresh (value: string): Promise<string> {
+      return await Promise.resolve('any_value')
+    }
+  }
+  return new DecrypterStub()
+}
+
+const makeLoadAccountByRefreshTokenRepo = (): LoadAccountByRefreshTokenRepository => {
+  class LoadAccountByRefreshTokenRepoStub implements LoadAccountByRefreshTokenRepository {
+    async loadByRefreshToken (refreshToken: string): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByRefreshTokenRepoStub()
+}
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+})
