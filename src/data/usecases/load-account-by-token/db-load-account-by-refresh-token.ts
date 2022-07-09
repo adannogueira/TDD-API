7119f6630bd52ee5179cc4af8bef52367e30b13c
Adan Nogueira <adan.nogueira@gmail.com>
2022-07-09 04:38:44 -0300

    feat: ensure db load account by refresh token calls load account by refresh token repo with correct values

--- src/data/usecases/load-account-by-token/db-load-account-by-refresh-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-refresh-token.ts
@@ -1,14 +1,19 @@
 import { LoadAccountByRefreshToken } from '../../../domain/usecases/load-account-by-refresh-token'
 import { RefreshDecrypter } from '../../protocols/criptography/refresh-decrypter'
+import { LoadAccountByRefreshTokenRepository } from '../../protocols/db/account/load-account-by-refresh-token-repository'
 import { AccountModel } from '../add-account/db-add-account-protocols'
 
 export class DbLoadAccountByRefreshToken implements LoadAccountByRefreshToken {
   constructor (
-    private readonly decrypter: RefreshDecrypter
+    private readonly decrypter: RefreshDecrypter,
+    private readonly loadAccountByRefreshTokenRepository: LoadAccountByRefreshTokenRepository
   ) {}
 
   async load (refreshToken: string): Promise<AccountModel> {
-    await this.decrypter.decryptRefresh(refreshToken)
+    const token = await this.decrypter.decryptRefresh(refreshToken)
+    if (token) {
+      await this.loadAccountByRefreshTokenRepository.loadByRefreshToken(refreshToken)
+    }
     return null
   }
 }
