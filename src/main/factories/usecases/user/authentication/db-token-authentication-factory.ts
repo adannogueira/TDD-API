40ae21654dd147e96e7a09bccdfff5be45899177
Adan Nogueira <adan.nogueira@gmail.com>
2022-07-09 08:46:21 -0300

    feat: add db token authentication factory

---
+++ src/main/factories/usecases/user/authentication/db-token-authentication-factory.ts
@@ -0,0 +1,27 @@
+import { DbAuthentication } from '../../../../../data/usecases/authentication/db-authentication'
+import { TokenAuthentication } from '../../../../../domain/usecases/token-authentication'
+import { BcryptAdapter } from '../../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { UuidAdapter } from '../../../../../infra/criptography/uuid-adapter/uuid-adapter'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../../config/env'
+
+export const makeDbTokenAuthentication = (): TokenAuthentication => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(
+    env.jwtSecret,
+    env.accessTokenExpiration,
+    env.refreshTokenExpiration
+  )
+  const accountMongoRepository = new AccountMongoRepository()
+  const idGenerator = new UuidAdapter()
+  return new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository,
+    accountMongoRepository,
+    idGenerator
+  )
+}
