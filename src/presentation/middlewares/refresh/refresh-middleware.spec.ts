48badd25c3a61378e9128e8e7ccdc0d355edd189
Adan Nogueira <adan.nogueira@gmail.com>
2022-07-09 09:42:03 -0300

    test: ensure refresh middleware returns 401 when accessToken is not expired

--- src/presentation/middlewares/refresh/refresh-middleware.spec.ts
+++ src/presentation/middlewares/refresh/refresh-middleware.spec.ts
@@ -1,5 +1,6 @@
 import { unauthorized } from '../../helpers/http/http-helper'
 import { RefreshMiddleware } from './refresh-middleware'
+import { HttpRequest, AccessDecrypter } from './refresh-middleware-protocols'
 
 describe('Refresh Middleware', () => {
   test('Should return 401 if no token exists in headers', async () => {
@@ -27,15 +28,40 @@ describe('Refresh Middleware', () => {
     })
     expect(httpResponse).toEqual(unauthorized())
   })
+
+  test('Should return 401 if accessToken is not expired', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(unauthorized())
+  })
 })
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': 'any_access_token',
+    'x-refresh-token': 'any_refresh_token'
+  }
+})
+
+const makeAccessDecrypter = (): AccessDecrypter => {
+  class AccessDecrypterStub implements AccessDecrypter {
+    async decrypt (value: string): Promise<string> {
+      return await Promise.resolve('any_value')
+    }
+  }
+  return new AccessDecrypterStub()
+}
 
 interface sutTypes {
   sut: RefreshMiddleware
+  accessDecrypterStub: AccessDecrypter
 }
 
 const makeSut = (): sutTypes => {
-  const sut = new RefreshMiddleware()
+  const accessDecrypterStub = makeAccessDecrypter()
+  const sut = new RefreshMiddleware(accessDecrypterStub)
   return {
-    sut
+    sut,
+    accessDecrypterStub
   }
 }
