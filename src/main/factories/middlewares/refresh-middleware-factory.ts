2c769d9b4b343c0008bf6a917c878433692dc6ea
Adan Nogueira <adan.nogueira@gmail.com>
2022-07-09 10:14:41 -0300

    feat: ensure refresh route returns 401 when refresh token does not exist

---
+++ src/main/factories/middlewares/refresh-middleware-factory.ts
@@ -0,0 +1,7 @@
+import { RefreshMiddleware } from '../../../presentation/middlewares/refresh/refresh-middleware'
+import { Middleware } from '../../../presentation/protocols'
+import { makeDecrypterFactory } from '../usecases/user/criptography/decrypter-factory'
+
+export const makeRefreshMiddleware = (): Middleware => {
+  return new RefreshMiddleware(makeDecrypterFactory())
+}
---
+++ src/main/factories/usecases/user/criptography/decrypter-factory.ts
@@ -0,0 +1,11 @@
+import { AccessDecrypter } from '../../../../../data/protocols/criptography/access-decrypter'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import env from '../../../../config/env'
+
+export const makeDecrypterFactory = (): AccessDecrypter => {
+  return new JwtAdapter(
+    env.jwtSecret,
+    env.accessTokenExpiration,
+    env.refreshTokenExpiration
+  )
+}
