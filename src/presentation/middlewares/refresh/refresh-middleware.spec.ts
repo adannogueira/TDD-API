a6cdb1a46844a7c2478fe47829acbeb629246ecf
Adan Nogueira <adan.nogueira@gmail.com>
2022-07-09 09:23:29 -0300

    test: ensure refresh middleware returns 401 if no access token exists

--- src/presentation/middlewares/refresh/refresh-middleware.spec.ts
+++ src/presentation/middlewares/refresh/refresh-middleware.spec.ts
@@ -7,6 +7,16 @@ describe('Refresh Middleware', () => {
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(unauthorized())
   })
+
+  test('Should return 401 if no x-access-token exists in headers', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      headers: {
+        'x-refresh-token': 'any_refresh_token'
+      }
+    })
+    expect(httpResponse).toEqual(unauthorized())
+  })
 })
 
 interface sutTypes {
