c4e88b220a97f8cb878f34da4a7a125f70496292
Adan Nogueira <adan.nogueira@gmail.com>
2022-07-09 08:45:57 -0300

    feat: add db load account by refresh token factory

---
+++ src/main/factories/usecases/user/load-account-by-token/db-load-account-by-refresh-token-factory.ts
@@ -0,0 +1,18 @@
+import { DbLoadAccountByRefreshToken } from '../../../../../data/usecases/load-account-by-token/db-load-account-by-refresh-token'
+import { LoadAccountByRefreshToken } from '../../../../../domain/usecases/load-account-by-refresh-token'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../../config/env'
+
+export const makeDbLoadAccountByRefreshToken = (): LoadAccountByRefreshToken => {
+  const jwtAdapter = new JwtAdapter(
+    env.jwtSecret,
+    env.accessTokenExpiration,
+    env.refreshTokenExpiration
+  )
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbLoadAccountByRefreshToken(
+    jwtAdapter,
+    accountMongoRepository
+  )
+}
