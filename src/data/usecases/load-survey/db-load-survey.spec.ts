693bc42b223968f03791b998d0f226be0e889aee
Adan Nogueira <adan.nogueira@gmail.com>
2022-09-01 08:43:05 -0300

    refactor: move sut creation to a helper function

--- src/data/usecases/load-survey/db-load-survey.spec.ts
+++ src/data/usecases/load-survey/db-load-survey.spec.ts
@@ -4,19 +4,31 @@ import { DbLoadSurvey } from './db-load-survey'
 
 describe('DbLoadSurvey', () => {
   test('Should call LoadSurveyRepository', async () => {
-    class LoadSurveyRepositoryStub implements LoadSurveyRepository {
-      async loadAll (): Promise<SurveyModel[]> {
-        return await Promise.resolve(makeFakeSurveys())
-      }
-    }
-    const loadSurveyRepositoryStub = new LoadSurveyRepositoryStub()
+    const { sut, loadSurveyRepositoryStub } = makeSut()
     const loadAllSpy = jest.spyOn(loadSurveyRepositoryStub, 'loadAll')
-    const sut = new DbLoadSurvey(loadSurveyRepositoryStub)
     await sut.load()
     expect(loadAllSpy).toHaveBeenCalled()
   })
 })
 
+const makeSut = (): SutTypes => {
+  const loadSurveyRepositoryStub = makeLoadSurveyRepositoryStub()
+  const sut = new DbLoadSurvey(loadSurveyRepositoryStub)
+  return { sut, loadSurveyRepositoryStub }
+}
+
+interface SutTypes {
+  sut: DbLoadSurvey
+  loadSurveyRepositoryStub: LoadSurveyRepository
+}
+const makeLoadSurveyRepositoryStub = (): LoadSurveyRepository => {
+  class LoadSurveyRepositoryStub implements LoadSurveyRepository {
+    async loadAll (): Promise<SurveyModel[]> {
+      return await Promise.resolve(makeFakeSurveys())
+    }
+  }
+  return new LoadSurveyRepositoryStub()
+}
 const makeFakeSurveys = (): SurveyModel[] => ([{
   id: 'any_id',
   question: 'any_question',
