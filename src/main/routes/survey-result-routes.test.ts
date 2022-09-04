88064aabc272e628897f0ed5e8562744cae38494
Adan Nogueira <adan.nogueira@gmail.com>
2022-09-04 17:48:48 -0300

    test: ensure save survey result route returns 200 if user is authorized

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,7 +1,28 @@
 import app from '$/main/config/app'
+import env from '$/main/config/env'
+import { sign } from 'jsonwebtoken'
+import { Collection } from 'mongodb'
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Result Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL!)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await surveyCollection.deleteMany({})
+  })
   describe('[PUT] /surveys/:surveyId/results', () => {
     test('Should return 403 when user is not authorized', async () => {
       await request(app)
@@ -11,5 +32,51 @@ describe('Survey Result Routes', () => {
         })
         .expect(403)
     })
+
+    test('Should return 200 on save survey result when user is authorized', async () => {
+      const survey = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const accessToken = await makeUserToken()
+      await request(app)
+        .put(`/api/surveys/${survey.insertedId.toString()}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
+    })
   })
 })
+
+const makeUserToken = async (
+  { admin, expired }: { admin?: boolean, expired?: boolean } = {}
+): Promise<string> => {
+  const user = await accountCollection.insertOne({
+    name: 'John Doe',
+    email: 'john@mail.com',
+    password: 'any_password',
+    role: admin ? 'admin' : undefined
+  })
+  const id = user.insertedId.toString()
+  const accessToken = sign(
+    { id },
+    env.jwtSecret,
+    { expiresIn: expired ? '0s' : '5m' }
+  )
+  await accountCollection.updateOne({
+    _id: user.insertedId
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
