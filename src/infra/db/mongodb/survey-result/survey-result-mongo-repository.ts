088038bde7b30e6e31a3ec28e3ce084d0654956b
Adan Nogueira <adan.nogueira@gmail.com>
2022-09-04 15:31:35 -0300

    feat: ensure survey result mongo repository inserts new seuvey result when it does not exist

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,22 @@
+import {
+  SaveSurveyResultModel,
+  SaveSurveyResultRepository,
+  SurveyResultModel
+} from '$/data/usecases/save-survey-result/save-survey-result-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save ({ surveyId, accountId, answer, date }: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const { value } = await surveyResultCollection.findOneAndUpdate({
+      surveyId,
+      accountId
+    }, {
+      $set: { answer, date }
+    }, {
+      upsert: true,
+      returnDocument: 'after'
+    })
+    return MongoHelper.map<SurveyResultModel>(value)
+  }
+}
