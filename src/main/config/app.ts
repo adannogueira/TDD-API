25036e4196842da4865debcd2363f305c8dd5baf
Adan Nogueira <adan.nogueira@gmail.com>
2022-09-06 10:22:23 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,9 @@
+import setupSwagger from './config-swagger'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import express from 'express'
-
 const app = express()
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'TDD API',
+    description: 'API construída usando TDD e técnicas de clean code',
+    version: '1.0.0'
+  }
+}
