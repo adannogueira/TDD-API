35faa9484e290347b96c0d331d03a7194cac9bd9
Adan Nogueira <adan.nogueira@gmail.com>
2022-09-06 08:59:22 -0300

    fix: wrong variable name

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
@@ -3,12 +3,12 @@ import {
   ValidationComposite,
   RequiredFieldValidation
 } from '$/validation/validators'
-import { mockAddSurveyValidation } from './add-survey-validation-factory'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
 jest.mock('$/validation/validators/validation-composite')
 
 describe('Add Survey Validation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
-    mockAddSurveyValidation()
+    makeAddSurveyValidation()
     const validations: Validation[] = []
     for (const field of ['question', 'answers']) {
       validations.push(new RequiredFieldValidation(field))
