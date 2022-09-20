c618289f98c03548a2272d6ac95332d31f2286ab
Adan Nogueira <adan.nogueira@gmail.com>
2022-09-20 08:34:50 -0300

    refactor: move sut creation to a helper function

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,11 +1,10 @@
 import { mockLoadSurveyById } from '$/presentation/test'
 import { LoadSurveyResultController } from './load-survey-result-controller'
-import { HttpRequest } from './load-survey-result-protocols'
+import { HttpRequest, LoadSurveyById } from './load-survey-result-protocols'
 
 describe('LoadSurveyResultController', () => {
   test('Should call LoadSurveyById with correct values', async () => {
-    const loadSurveyByIdStub = mockLoadSurveyById()
-    const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+    const { sut, loadSurveyByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
     await sut.handle(mockRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
@@ -17,3 +16,14 @@ const mockRequest = (): HttpRequest => ({
     surveyId: 'any_id'
   }
 })
+
+type SutTypes = {
+  sut: LoadSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById()
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+  return { sut, loadSurveyByIdStub }
+}
