6958a19f6df8a621692340f3412faa62f1ae1455
Adan Nogueira <adan.nogueira@gmail.com>
2022-10-15 18:06:46 -0300

    refactor: use omit to save params data

--- src/domain/usecases/account/add-account.ts
+++ src/domain/usecases/account/add-account.ts
@@ -1,12 +1,10 @@
+import { AccountModel } from '../../models/account'
+
 export interface AddAccount {
   add: (account: AddAccount.Params) => Promise<AddAccount.Result>
 }
 
 export namespace AddAccount {
-  export type Params = {
-    name: string
-    email: string
-    password: string
-  }
+  export type Params = Omit<AccountModel, 'id'>
   export type Result = boolean
 }
