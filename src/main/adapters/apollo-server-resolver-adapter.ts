388ee36b009dbe326958025867e554c1041fb4a4
Adan Nogueira <adan.nogueira@gmail.com>
2022-10-16 17:10:41 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,6 +1,14 @@
 import { Controller } from '$/presentation/protocols'
+import { ApolloError, AuthenticationError, ForbiddenError, UserInputError } from 'apollo-server-express'
 
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const response = await controller.handle(args)
-  return response.body
+  switch (response.statusCode) {
+    case 200:
+    case 204: return response.body
+    case 400: throw new UserInputError(response.body.message)
+    case 401: throw new AuthenticationError(response.body.message)
+    case 403: throw new ForbiddenError(response.body.message)
+    default: throw new ApolloError(response.body.message)
+  }
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,8 +1,30 @@
 import resolvers from '$/main/graphql/resolvers'
 import typeDefs from '$/main/graphql/type-defs'
 import { ApolloServer } from 'apollo-server-express'
+import { GraphQLError } from 'graphql'
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    response.http.status = checkError(error.originalError.name || error.name)
+  })
+}
+
+const checkError = (errorName: string): boolean => {
+  const errors = {
+    UserInputError: 400,
+    AuthenticationError: 401,
+    ForbiddenError: 403
+  }
+  return errors[errorName] || 500
+}
 
 export const setupApolloServer = (): ApolloServer => new ApolloServer({
   resolvers,
-  typeDefs
+  typeDefs,
+  plugins: [{
+    requestDidStart: async () => ({
+      willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
+    })
+  }]
 })
