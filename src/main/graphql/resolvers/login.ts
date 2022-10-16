180629b70b856b59e15dc531c0e5cf013a89262b
Adan Nogueira <adan.nogueira@gmail.com>
2022-10-16 17:18:20 -0300

    feat: add signup mutation

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,13 @@
 import { adaptResolver } from '$/main/adapters/apollo-server-resolver-adapter'
 import { makeLoginController } from '$/main/factories/controllers/user/login/login-controller-factory'
+import { makeSignUpController } from '../../factories/controllers/user/signup/signup-controller-factory'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => await adaptResolver(makeLoginController(), args)
+  },
+
+  Mutation: {
+    signup: async (parent: any, args: any) => await adaptResolver(makeSignUpController(), args)
   }
 }
