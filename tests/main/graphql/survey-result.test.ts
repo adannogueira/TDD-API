c6e862e1573ead5daa8d6987e6de9d3f7025d1e1
Adan Nogueira <adan.nogueira@gmail.com>
2022-11-02 15:12:39 -0300

    test: ensure surveyResult query returns SurveyResults on success

---
+++ tests/main/graphql/survey-result.test.ts
@@ -0,0 +1,94 @@
+import { MongoHelper } from '$/infra/db/mongodb/helpers'
+import env from '$/main/config/env'
+import { setupApolloServer } from '$/main/graphql/apollo/apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import { sign } from 'jsonwebtoken'
+import { Collection } from 'mongodb'
+
+let apolloServer: ApolloServer
+let accountCollection: Collection
+let surveyCollection: Collection
+
+describe('SurveyResult GraphQL', () => {
+  beforeAll(async () => {
+    apolloServer = setupApolloServer()
+    await MongoHelper.connect(process.env.MONGO_URL!)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await surveyCollection.deleteMany({})
+  })
+  describe('SurveyResult Query', () => {
+    const query = `
+      query surveyResult ($surveyId: String!) {
+        surveyResult (surveyId: $surveyId) {
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }
+    `
+    it('Should return surveyResults', async () => {
+      const token = await makeUserToken()
+      const now = new Date()
+      const survey = await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: now
+      }])
+      const response = await apolloServer.executeOperation(
+        { query, variables: { surveyId: survey.insertedIds[0].toString() } },
+        { req: { headers: { 'x-access-token': token } } } as any
+      )
+      const { surveyResult } = response.data
+      expect(surveyResult.question).toBe('any_question')
+      expect(surveyResult.date).toStrictEqual(now)
+      expect(surveyResult.answers).toEqual([{
+        answer: 'any_answer',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }])
+    })
+  })
+})
+
+const makeUserToken = async (
+  { admin, expired }: { admin?: boolean, expired?: boolean } = {}
+): Promise<string> => {
+  const user = await accountCollection.insertOne({
+    name: 'John Doe',
+    email: 'john@mail.com',
+    password: 'any_password',
+    role: admin ? 'admin' : undefined
+  })
+  const id = user.insertedId.toString()
+  const accessToken = sign(
+    { id },
+    env.jwtSecret,
+    { expiresIn: expired ? '0s' : '5m' }
+  )
+  await accountCollection.updateOne({
+    _id: user.insertedId
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
